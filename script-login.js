4dcf7dc31620128f4b6ceffb3b18fd5ff22ee870
SadraDinAziziMuhammad <sadraazizi1305@gmail.com>
2023-10-19 21:23:12 +0700

    localstorage

--- script-login.js
+++ script-login.js
@@ -1,20 +1,24 @@
 const btnlogin = document.getElementById('login');
 
-btnlogin.addEventListener('click', function(event){
+btnlogin.addEventListener('click', function (event) {
     event.preventDefault();
 
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
 
+    localStorage.setItem("Email", email);
+    localStorage.setItem("Password", password);
+
+
 
     fetch('https://652ff6b96c756603295e01fb.mockapi.io/regist')
-    .then(res=>res.json())
-  .then(res=>{
-    console.log(res)
-    const userExist = res.find(user=>user.email === email && user.password === password)
-    console.log(userExist);
-    if(userExist===undefined){
-        console.log("Email atau Password Salah")
-    }
-  })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            const userExist = res.find(user => user.email === email && user.password === password)
+            console.log(userExist);
+            if (userExist === undefined) {
+                console.log("Email atau Password Salah")
+            }
+        })
 })
