98773099c3b16e9c76ead488b6bbeb3d3c45e7b9
Hellodyonz <hellodyonz@gmail.com>
2023-10-20 03:05:43 +0700

    pencarian mentor done

---
+++ script/home.js
@@ -0,0 +1,34 @@
+let select = document.getElementById('select');
+let listSearch = document.getElementById('list-search');
+let selectText = document.getElementById('select-text');
+let inputField = document.getElementById('search-input');
+let options = document.getElementsByClassName('options');
+
+
+// mengatur pencarian home
+
+select.onclick = function(){
+    listSearch.classList.toggle("open");
+}
+
+for(option of options){
+    option.onclick = function(){
+        selectText.innerHTML = this.innerHTML;
+        inputField.placeholder = "Yuk cari " + selectText.innerHTML +" faforitmu"
+    }
+}
+
+// oper nilai dari home
+
+let searchBtn = document.getElementById('search-btn');
+
+searchBtn.addEventListener('click',function(){
+
+    inputSearch = document.getElementById('search-input').value;
+
+    const url = `carimentor.html?search=${inputSearch}`;
+
+    window.location.href = url;
+
+})
+
